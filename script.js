c837b9dd8ecde3dca4a3cbd73321e31b714e0c00
Robert Booth <boothr16@gmail.com>
2021-06-09 18:49:15 -0400

    Added computerPlay() function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+// randomly selects rock, paper, or scissors
+function computerPlay() {
+    const choices = ["rock", "paper", "scissors"];
+    const min = 0;
+    const max = choices.length - 1;
+    const rand = Math.floor(Math.random() * (max - min + 1)) + min; // true random, inclusive range
+    console.log(choices[rand]);
+    return choices[rand];
+}
