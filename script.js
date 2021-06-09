80d33d2092f43bc459a5ef6ad150732058b1b9a2
Robert Booth <boothr16@gmail.com>
2021-06-09 19:39:47 -0400

    Added code to create new div with scores

--- script.js
+++ script.js
@@ -4,55 +4,56 @@ function computerPlay() {
     const min = 0;
     const max = choices.length - 1;
     const rand = Math.floor(Math.random() * (max - min + 1)) + min; // true random, inclusive range
-    //console.log(choices[rand]);
     return choices[rand];
 }
 
+function playRound(userChoice) {
+    let computerChoice = computerPlay();
+    if (userChoice == computerChoice)
+    alert("Tie!");
+    else {
+        if (computerChoice == "r") {
+            if (userChoice == "p") {
+                alert("Player wins!");
+                return true;
+            }
+            else if (userChoice == "s"){
+                alert("PC wins!");
+                return false;                
+            }
+        }
+        else if (computerChoice == "p") {
+            if (userChoice == "s") {
+                alert("Player wins!");
+                return true
+            }
+            else if (userChoice == "r") {
+                alert("PC wins!");
+                return false
+            }           
+        }
+        else {
+            if (userChoice == "r") {
+                alert("Player wins!");
+                return true;
+            }
+            else if (userChoice == "p") {
+                alert("PC wins!");
+                return false;
+            }           
+        }
+    }
+};
+
+// add functionality to buttons and play single round on-click
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        let userChoice = button.id;
-        let computerChoice = computerPlay();
-        if (userChoice == computerChoice)
-        alert("Tie!");
-        else {
-            if (computerChoice == "r") {
-                if (userChoice == "p") {
-                    alert("Player wins!");
-                    return true;
-                }
-                else if (userChoice == "s"){
-                    alert("PC wins!");
-                    return false;                
-                }
-                else
-                    alert("Invalid input.");
-            }
-            else if (computerChoice == "p") {
-                if (userChoice == "s") {
-                    alert("Player wins!");
-                    return true
-                }
-                else if (userChoice == "r") {
-                    alert("PC wins!");
-                    return false
-                }
-                else
-                    alert("Invalid input.");            
-            }
-            else {
-                if (userChoice == "r") {
-                    alert("Player wins!");
-                    return true;
-                }
-                else if (userChoice == "p") {
-                    alert("PC wins!");
-                    return false;
-                }
-                else
-                    alert("Invalid input.");            
-            }
-        }
+        playRound(button.id);
     });
 });
 
+// create div to display and update score
+const container = document.querySelector('#btn-container');
+const scores = document.createElement('div');
+scores.classList.add('scores');
